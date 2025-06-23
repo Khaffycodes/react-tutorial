efeebcb74e9067dfab44db26820efdcf1b6528b7
Salau Kafayat <shilekhaffy@gmail.com>
2025-06-23 22:15:21 +0100

    output image dynamically

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,28 +1,6 @@
-// Exercise
-// function MainGoal() {
-//   return (
-//     <div>
-//       <h1>Main goal</h1>
-//       <p>
-//         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel aut porro
-//         modi quas consequuntur ut eum deserunt ipsam, veniam, quod nisi eveniet
-//         harum temporibus numquam assumenda iusto iure similique corrupti.
-//       </p>
-//       <p>
-//         <strong>Lorem ipsum </strong>dolor sit amet consectetur adipisicing
-//         elit. Atque ab quibusdam molestias?
-//       </p>
-//       <p>
-//         {' '}
-//         <strong>Lorem,</strong> ipsum dolor sit amet consectetur adipisicing
-//         elit.
-//       </p>
-//     </div>
-//   );
-// }
+import reactImg from './assets/react-core-concepts.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
@@ -32,7 +10,7 @@ function Header() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
@@ -55,3 +33,26 @@ function App() {
 }
 
 export default App;
+
+// Exercise
+// function MainGoal() {
+//   return (
+//     <div>
+//       <h1>Main goal</h1>
+//       <p>
+//         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel aut porro
+//         modi quas consequuntur ut eum deserunt ipsam, veniam, quod nisi eveniet
+//         harum temporibus numquam assumenda iusto iure similique corrupti.
+//       </p>
+//       <p>
+//         <strong>Lorem ipsum </strong>dolor sit amet consectetur adipisicing
+//         elit. Atque ab quibusdam molestias?
+//       </p>
+//       <p>
+//         {' '}
+//         <strong>Lorem,</strong> ipsum dolor sit amet consectetur adipisicing
+//         elit.
+//       </p>
+//     </div>
+//   );
+// }
