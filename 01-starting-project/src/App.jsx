ba79c2e4eb718490fadb7f10a2ae5230348dd195
Salau Kafayat <shilekhaffy@gmail.com>
2025-06-24 16:52:13 +0100

    styling components

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,66 +1,19 @@
-import reactImg from './assets/react-core-concepts.png';
-import { CORE_CONCEPTS } from './data';
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcepts.jsx';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-// function CoreConcept(props) {
-//   return (
-//     <li>
-//       <img src={props.image} alt={props.title} />
-//       <h3>{props.title}</h3>
-//       <p>{props.description}</p>
-//     </li>
-//   );
-// }
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
-
-function CourseGoal({ title, description }) {
-  return (
-    <li>
-      <h2>{title}</h2>
-      <p>{description}</p>
-    </li>
-  );
-}
 function App() {
   return (
     <div>
       {/* <MainGoal /> */}
       <Header />
       <main>
-        <section id="core-concepts">
+        {/* <section id="core-concepts">
           <ul>
             <CourseGoal title="Learn react" description="in-depth" />
             <CourseGoal title="exercise" description="done by me" />
           </ul>
-        </section>
+        </section> */}
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
@@ -80,26 +33,3 @@ function App() {
 }
 
 export default App;
-
-// Exercise
-// function MainGoal() {
-//   return (
-//     <div>
-//       <h1>Main goal</h1>
-//       <p>
-//         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel aut porro
-//         modi quas consequuntur ut eum deserunt ipsam, veniam, quod nisi eveniet
-//         harum temporibus numquam assumenda iusto iure similique corrupti.
-//       </p>
-//       <p>
-//         <strong>Lorem ipsum </strong>dolor sit amet consectetur adipisicing
-//         elit. Atque ab quibusdam molestias?
-//       </p>
-//       <p>
-//         {' '}
-//         <strong>Lorem,</strong> ipsum dolor sit amet consectetur adipisicing
-//         elit.
-//       </p>
-//     </div>
-//   );
-// }
---
+++ 01-starting-project/src/components/exercise.jsx
@@ -0,0 +1,32 @@
+// Exercise
+// function MainGoal() {
+//   return (
+//     <div>
+//       <h1>Main goal</h1>
+//       <p>
+//         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel aut porro
+//         modi quas consequuntur ut eum deserunt ipsam, veniam, quod nisi eveniet
+//         harum temporibus numquam assumenda iusto iure similique corrupti.
+//       </p>
+//       <p>
+//         <strong>Lorem ipsum </strong>dolor sit amet consectetur adipisicing
+//         elit. Atque ab quibusdam molestias?
+//       </p>
+//       <p>
+//         {' '}
+//         <strong>Lorem,</strong> ipsum dolor sit amet consectetur adipisicing
+//         elit.
+//       </p>
+//     </div>
+//   );
+// }
+
+//exercise
+// function CourseGoal({ title, description }) {
+//   return (
+//     <li>
+//       <h2>{title}</h2>
+//       <p>{description}</p>
+//     </li>
+//   );
+// }
