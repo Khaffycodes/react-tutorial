09ddaf426faf22f37a2b046a5f8f312b7baedd3e
Salau Kafayat <shilekhaffy@gmail.com>
2025-07-31 21:16:23 +0100

    tested

--- tic-tac-toe-starting-prj/src/components/GameBoard.jsx
+++ tic-tac-toe-starting-prj/src/components/GameBoard.jsx
@@ -37,19 +37,3 @@ export default function GameBoard() {
     </ol>
   );
 }
-
-<ol id="game-board">
-  {gameBoard.map((row, rowIndex) => (
-    <li key={rowIndex}>
-      <ol>
-        {row.map((playerSymbol, colIndex) => (
-          <li key={colIndex}>
-            <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
-              {playerSymbol}
-            </button>
-          </li>
-        ))}
-      </ol>
-    </li>
-  ))}
-</ol>;
