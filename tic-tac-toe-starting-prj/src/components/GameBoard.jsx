57ddff70373d7ddb4d2b94651a2e75b0fa24c38e
Salau Kafayat <shilekhaffy@gmail.com>
2025-08-07 21:01:41 +0100

    strting over

--- tic-tac-toe-starting-prj/src/components/GameBoard.jsx
+++ tic-tac-toe-starting-prj/src/components/GameBoard.jsx
@@ -20,18 +20,19 @@ export default function GameBoard({ onSelectSquare }) {
   //}
 
   return (
-    <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
-        <li key={rowIndex}>
-          <ol>
-            {row.map((playerSymbol, colIndex) => (
-              <li key={colIndex}>
-                <button onClick={onSelectSquare}>{playerSymbol}</button>
-              </li>
-            ))}
-          </ol>
-        </li>
-      ))}
-    </ol>
+    <div></div>
+    //   <ol id="game-board">
+    //     {gameBoard.map((row, rowIndex) => (
+    //       <li key={rowIndex}>
+    //         <ol>
+    //           {row.map((playerSymbol, colIndex) => (
+    //             <li key={colIndex}>
+    //               <button onClick={onSelectSquare}>{playerSymbol}</button>
+    //             </li>
+    //           ))}
+    //         </ol>
+    //       </li>
+    //     ))}
+    //   </ol>
   );
 }
