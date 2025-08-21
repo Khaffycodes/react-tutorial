e1651cfc0d42fa03c272b7bac9e777404ac00934
Salau Kafayat <shilekhaffy@gmail.com>
2025-08-21 14:39:04 +0100

    added header

--- investment app/src/components/Header.jsx
+++ investment app/src/components/Header.jsx
@@ -1,3 +1,10 @@
 import investmentImg from '../assets/investment-calculator-logo.png';
 
-export default function Header() {}
+export default function Header() {
+  return (
+    <header id="header">
+      <img src={investmentImg} alt="app logo" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}
