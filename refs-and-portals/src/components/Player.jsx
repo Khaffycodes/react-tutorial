a366bce8d4e2cfbe2a54545bc3bccc607983d663
Salau Kafayat <shilekhaffy@gmail.com>
2025-09-03 14:00:11 +0100

    userInput with ref

--- refs-and-portals/src/components/Player.jsx
+++ refs-and-portals/src/components/Player.jsx
@@ -1,11 +1,52 @@
+import { useRef, useState } from 'react';
+
+//Managing userInput with ref
+
 export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>
+        Welcome {enteredPlayerName ? enteredPlayerName : 'unknown entity'}
+      </h2>
+      {/* <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2> */}
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
 }
+
+//Managing userInput with state
+
+// export default function Player() {
+//   const [enteredPlayerName, setEnteredPlayerName] = useState('');
+//   const [submitted, setSubmitted] = useState(false);
+
+//   function handleChange(event) {
+//     setSubmitted(false);
+//     setEnteredPlayerName(event.target.value);
+//   }
+
+//   function handleClick() {
+//     setSubmitted(true);
+//   }
+
+//   return (
+//     <section id="player">
+//       <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+//       <p>
+//         <input type="text" onChange={handleChange} value={enteredPlayerName} />
+//         <button onClick={handleClick}>Set Name</button>
+//       </p>
+//     </section>
+//   );
+// }
