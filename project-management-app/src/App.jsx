4365c7b6ed17eb9daf00dff09541986d5483386d
Salau Kafayat <shilekhaffy@gmail.com>
2025-09-10 22:09:30 +0100

    managing state btwn comps

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
 import ProjectSidebar from './components/ProjectSidebar';
+
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
